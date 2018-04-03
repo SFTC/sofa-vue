06e8f0e644f8dfed7e122271e3cfe757d48830dc
lichun <chunli@sf-express.com>
2018-04-03 16:02:12 +0800

    add unit test

--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -1,21 +1,78 @@
 <template>
-  <div
+  <button
     class="sofa-button"
     @click="handleClick"
+    :disabled="buttonDisabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+    :class="[
+      type ? 'sofa-button--' + type : '',
+      buttonSize ? 'sofa-button--' + buttonSize : '',
+      {
+        'is-disabled': buttonDisabled,
+        'is-loading': loading,
+        'is-plain': plain,
+        'is-round': round,
+        'is-circle': circle
+      }
+    ]"
   >
-    very good
-    <slot></slot>
-  </div>
+    <i class="sofa-icon-loading" v-if="loading"></i>
+    <i :class="icon" v-if="icon && !loading"></i>
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
 </template>
-
 <script>
-export default {
-  name: 'sofa-button',
-  methods: {
-    handleClick(event) {
-      this.$emit('click', event);
-    },
-  },
-};
-</script>
+  export default {
+    name: 'SofaButton',
 
+    inject: {
+      sofaForm: {
+        default: ''
+      },
+      sofaFormItem: {
+        default: ''
+      }
+    },
+
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      size: String,
+      icon: {
+        type: String,
+        default: ''
+      },
+      nativeType: {
+        type: String,
+        default: 'button'
+      },
+      loading: Boolean,
+      disabled: Boolean,
+      plain: Boolean,
+      autofocus: Boolean,
+      round: Boolean,
+      circle: Boolean
+    },
+
+    computed: {
+      _sofaFormItemSize() {
+        return (this.sofaFormItem || {}).sofaFormItemSize;
+      },
+      buttonSize() {
+        return this.size || this._sofaFormItemSize;
+      },
+      buttonDisabled() {
+        return this.disabled || (this.sofaForm || {}).disabled;
+      }
+    },
+
+    methods: {
+      handleClick(evt) {
+        this.$emit('click', evt);
+      }
+    }
+  };
+</script>
