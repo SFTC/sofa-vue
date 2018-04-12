2779532bea7fa6a71aba15c878ebd2bfa1953184
Leona Yang <leonayang@sf-express.com>
2018-04-12 20:27:50 +0800

    add radio

--- site/src/App.vue
+++ site/src/App.vue
@@ -7,6 +7,7 @@
           <li><router-link to="/icon">Icon</router-link></li>
           <li><router-link to="/button">Button</router-link></li>
           <li><router-link to="/input">Input</router-link></li>
+          <li><router-link to="/radio">Radio</router-link></li>
         </ul>
       </nav>
       <div id="main">
---
+++ site/src/pages/Radio/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <comp-show-container>
+      <sofa-radio v-model="radio1" label="male">male</sofa-radio>
+      <sofa-radio v-model="radio1" label="female">female</sofa-radio>
+    </comp-show-container>
+
+    <comp-show-container>
+      <sofa-radio v-model="radio2" label="1" :disabled="true">male</sofa-radio>
+      <sofa-radio v-model="radio2" label="2" :disabled="true">female</sofa-radio>
+    </comp-show-container>
+  </div>
+</template>
+
+<script>
+import SofaRadio from 'packages/radio';
+import CompShowContainer from '../../components/CompShowContainer';
+
+export default {
+  name: 'ButtonPage',
+  components: {
+    CompShowContainer,
+    SofaRadio,
+  },
+  data() {
+    return {
+      radio1: 'male',
+      radio2: '1',
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
